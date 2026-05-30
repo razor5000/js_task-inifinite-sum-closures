e41ac9d31ea62092bea4e4922802e6c75f9eec7f
George Peydy <greenstarmain2gmail.com>
2026-05-30 16:46:56 +0300

    add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,21 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let countBank = 0;
+
+  return function add(...args) {
+    if (args.length === 0) {
+      const temp = countBank;
+
+      countBank = 0;
+
+      return temp;
+    }
+
+    countBank += args[0];
+
+    return add;
+  };
 }
 
 module.exports = makeInfinityAdder;
